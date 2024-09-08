70e5af002b0911e8e8def42a9daa626e459c37cb
yasin ziyai <yasinziyai@gmail.com>
2024-09-08 18:06:47 +0330

    debug

--- app/page.js
+++ app/page.js
@@ -135,8 +135,11 @@ const AiPage = () => {
       <div
         className={`
           ${loading ? "opacity-5" : ""}
-          fixed inset-x-0 mx-[16px]  bottom-[16px] sm:mx-auto sm:w-[496px] md:w-[584px] 
-          font-black  text-[#000000] md:bottom-[42px]    `}
+          fixed inset-x-0 mx-[16px] 
+          bottom-[16px] sm:mx-auto
+           sm:w-[496px] md:w-[584px] 
+          font-black  text-[#000000]
+           md:bottom-[42px]`}
       >
         <button
           type="submit"
